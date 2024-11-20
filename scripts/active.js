b1e299fa4eb7448f61996c251409768b6e3c9f3a
kravchenko.e <egor.kravchenko.work@gmail.com>
2024-11-20 13:41:49 +0300

    debug remotely

--- scripts/active.js
+++ scripts/active.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const currentPath = document.location.pathname;
   var menuItems = document.querySelectorAll(".topics .list-elem a");
+  console.log("current path: ", currentPath)
 
   menuItems.forEach((item) => {
+    console.log("item: ", item)
     const itemPath = item.href.replace(document.location.origin, "");
     if (itemPath === currentPath) {
       item.classList.add("active");
